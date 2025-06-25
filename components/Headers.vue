8a8894f4d25f59928338040a5d713c55bd1a760a
mahardikakdenie <kucingemas744@gmail.com>
2025-06-25 15:14:02 +0700

    login button

--- components/Headers.vue
+++ components/Headers.vue
@@ -2,6 +2,11 @@
     <header class="bg-white shadow py-4">
         <div class="mx-auto px-6 flex justify-between items-center">
             <img src="/logo.png" width="100" alt="">
+            <div>
+                <button class="bg-green-200 text-green-700 hover:bg-green-400 hover:text-white cursor-pointer text-sm rounded-xl px-6 py-1">
+                    Login
+                </button>
+            </div>
         </div>
     </header>
 </template>
